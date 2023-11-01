e384a93e0cf6be92edcb604847b52bf7107ff678
Rique <aguirree1985@gmail.com>
2023-11-01 10:43:44 -0700

    adds pseudo code

--- challenge.rb
+++ challenge.rb
@@ -1,1 +1,31 @@
 # Text-based Game Challenge
+# Escape room 
+#
+# Riddles
+    #How many letters are in "the alphabet"=11
+    #I have 6 eggs. broke 2, cooked2, and ate 2. How many eggs do I have
+    #spell name using telephone alphabet
+    #possible time limit
+
+#Pseudo Code:
+#have command to show input possibilities
+#You are in an escape room solve # riddles to escape
+# give user option to choose riddle
+    # print out riddle
+    # give user choice to engage riddle skip hint
+    # if chooses to engage 
+        # user input .gets
+    #case when user input = correct
+        #solved mesage
+    #case when user input != correct
+        #try again
+
+# method for each riddle
+# true/false switch for copleted tasks
+
+#when all switches are completed door opens 
+
+
+
+
+
